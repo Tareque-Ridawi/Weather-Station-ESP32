b8625ae5be8e9854a2a9950d1012c864c37d0745
Tareque Ridawi <165356207+Tareque-Ridawi@users.noreply.github.com>
2025-11-17 20:55:12 +0600

    Add  class to dashboard container

--- WebPage/dashboard.php
+++ WebPage/dashboard.php
@@ -12,7 +12,7 @@ $controls = $mysqli->query("SELECT * FROM controls WHERE id=1")->fetch_assoc();
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-<div class="container py-4">
+<div class="container py-4 ds-2">
 
   <div class="text-center mb-4">
     <h1 class="fw-bold">IoT Smart Weather Station</h1>
